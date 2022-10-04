6588bf30de6957b6e3349b2206c069406fddf897
ZairaHernandez <86864292+ZairaHernandez@users.noreply.github.com>
2022-10-03 21:01:54 -0500

    Delete Leccion06 directory

--- Leccion06/07-01-Funciones.js
+++
@@ -1,8 +0,0 @@
-miFuncion(4, 2);
-//Declaración de la función de suma de dos numeros
-function miFuncion(a, b) {
-    console.log("Suma: " + (a + b));
-}
-//Llamando a la función
-miFuncion(2, 3);
-
--- Leccion06/07-02-Return.js
+++
@@ -1,8 +0,0 @@
-//Declaración de la función
-function miFuncion(a, b) {
-     return a + b;
-}
-
-//Llamando a la función
-let resultado = miFuncion(4, 9);
-console.log(resultado);
