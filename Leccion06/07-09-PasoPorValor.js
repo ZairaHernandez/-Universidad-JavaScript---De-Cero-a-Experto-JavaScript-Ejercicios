79fb9331ce03e38ef5c31cb98b638bdd03b27752
ZairaHernandez <86864292+ZairaHernandez@users.noreply.github.com>
2022-10-03 20:53:37 -0500

    Leccion6

---
+++ Leccion06/07-09-PasoPorValor.js
@@ -0,0 +1,11 @@
+//Tipos de datos primitivos
+let x = 10;
+
+//Paso por valor(función)
+function cambiarValor(a) {
+    a = 20;
+}
+
+//Paso por valor
+cambiarValor(x);//10
+console.log(x); //console.log(a);
